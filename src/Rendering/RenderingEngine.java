add524a5b5da0418af84264201200eb563b2d7b4
privatesimmons <ddr.maier@gmail.com>
2014-08-26 23:18:23 +0200

    Have a display working, renderingEngine coming along

--- src/Rendering/RenderingEngine.java
+++ src/Rendering/RenderingEngine.java
@@ -1,12 +1,39 @@
 package Rendering;
 
+import GameObjects.GameObject;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.util.ArrayList;
+
 /**
  * Created by Damien on 24/8/2014.
  */
 public class RenderingEngine
 {
-    public RenderingEngine()
+
+    private Display m_Display;
+    private ArrayList<GameObject> m_objectsToBeRendered;
+    private BufferedImage m_image;
+    private byte[] m_pixelArray;
+
+    public RenderingEngine(Display display)
+    {
+        m_Display = display;
+        m_objectsToBeRendered = new ArrayList<GameObject>();
+
+        m_image = new BufferedImage(m_Display.getWidth(), m_Display.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        m_pixelArray = ((DataBufferByte)m_image.getRaster().getDataBuffer()).getData();
+    }
+
+    public void addToRender(GameObject go)
+    {
+        m_objectsToBeRendered.add(go);
+    }
+
+    public void render()
     {
 
     }
+
 }
