8481c723a20839ab5ae67862198f4623573576f3
privatesimmons <ddr.maier@gmail.com>
2014-08-31 18:49:17 +0200

    Changed the input system

--- src/GameObjects/Player.java
+++ src/GameObjects/Player.java
@@ -1,7 +1,14 @@
 package GameObjects;
 
+import Game.Input;
+
 /**
  * Created by damien on 2014/08/31.
  */
-public class Player {
+public class Player extends GameObject
+{
+    public void update(Input input , int delta)
+    {
+
+    }
 }
